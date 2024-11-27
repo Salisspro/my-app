569084571b8b9cb541cfb56ebe4f75d0ff5f5dc1
salisspro <salisspro@email.com>
2024-11-27 05:04:46 +0100

    first changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,24 @@
 
 
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Button, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const index = () => {
-  return (
-    <View style={styles.app}>
-      <Text>salisspro</Text>
-      <TouchableOpacity style={{backgroundColor: '#161620', padding: 15, width: 270, borderRadius: 5, margin: 20}}>
-        <Text style={{color: '#fff', textAlign: 'center'}}>Let get started</Text>
-      </TouchableOpacity>
-    </View>
-  )
+    return (
+        <View style={styles.app}>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end', margin: 20 }}>
+                <StatusBar backgroundColor='#161616'/>
+
+                <TouchableOpacity style={{ backgroundColor: '#161620', padding: 15, width: 270, borderRadius: 5, margin: 20 }}>
+                    <Text style={{ color: '#fff', textAlign: 'center' }}>Get started</Text>
+
+                </TouchableOpacity>
+            </View>
+
+                    
+        </View>
+    )
 }
 
 export default index
@@ -21,6 +28,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        
+
     }
 })
